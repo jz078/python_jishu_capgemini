005a01c31ee7471ec092c2ec442714f878d281b1
Jishu Mahato <jishumahato50@gmail.com>
2026-02-20 23:07:23 +0530

    Added f1.py

---
+++ for_loop_questions/f1.py
@@ -0,0 +1,23 @@
+# count how many vowels are present in a given string.
+
+import logging
+
+# Configure logging
+logging.basicConfig(
+    filename="f1.log",      
+    level=logging.INFO,          
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+s: str = str(input("Enter a string: "))
+
+logging.info("User entered string: %s", s)
+
+count: int = 0
+
+for ch in s:
+    if ch in "aeiouAEIOU":
+        count += 1
+        logging.info("Found vowel: %s", ch)
+
+logging.info("Total vowels: %d", count)
