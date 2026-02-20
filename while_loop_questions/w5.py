816bb7d6ad2058913fa53d3c9eff72ac751362f6
Jishu Mahato <jishumahato50@gmail.com>
2026-02-20 22:35:42 +0530

    Added w5.py

---
+++ while_loop_questions/w5.py
@@ -0,0 +1,23 @@
+# given a number, check whether it is a palindrome using a while loop.
+
+def reverse_number(num):
+    rev = 0
+
+    while(num > 0):
+        digit = num%10
+        rev *= 10
+        rev += digit
+        num //= 10
+
+    return rev
+        
+
+
+num = int(input("Enter a number : "))
+
+rev = reverse_number(num)
+
+if(rev == num):
+    print(num, " is palindrome")
+else:
+    print("not palindrome")
