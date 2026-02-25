62779acd8f2103e87d784f728b8c17cf50cb2d04
Jishu Mahato <jishumahato50@gmail.com>
2026-02-25 12:44:17 +0530

    sum-of-three-numbers-using-lambda.py

---
+++ Extras/sum-of-three-numbers-using-lambda.py
@@ -0,0 +1,33 @@
+# sum of three numbers using lambda expression
+
+# s = lambda a, b, c: a + b + c
+# print(s(10, 20, 30))
+
+
+
+
+# mymath.py
+    # def add(a, b):
+    #     return a+b
+    # def sub(a, b):
+    #     return a-b
+
+# c.py
+    # # import mymath as my
+    # from mymath import add
+    # print(add(2, 3))
+
+
+
+
+# x = 10
+# def show_value():
+#     global x
+#     x += 10
+#     print(x)
+
+# def value():
+#     print(x)
+
+# show_value()
+# value()
